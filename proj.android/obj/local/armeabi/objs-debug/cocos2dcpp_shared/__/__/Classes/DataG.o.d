5f213ae13d1771cd62e774f72b091d55433f52ee
maytang <michelle0530@gmail.com>
2016-09-28 19:50:48 +0800

    Take off useless compiler files.

--- proj.android/obj/local/armeabi/objs-debug/cocos2dcpp_shared/__/__/Classes/DataG.o.d
+++
@@ -1,2 +0,0 @@
-obj/local/armeabi/objs-debug/cocos2dcpp_shared/__/__/Classes/DataG.o: \
- jni/../../Classes/DataG.cpp
--- proj.android/obj/local/armeabi/objs/cocos2dcpp_shared/__/__/Classes/DataG.o.d
+++
@@ -1,2 +0,0 @@
-obj/local/armeabi/objs/cocos2dcpp_shared/__/__/Classes/DataG.o: \
- jni/../../Classes/DataG.cpp
